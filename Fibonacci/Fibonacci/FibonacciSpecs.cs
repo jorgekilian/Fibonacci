58399dccfea04f10b9e3580345b8f010c3affaf7
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-03-29 18:03:09 +0100

    create project

---
+++ Fibonacci/Fibonacci/FibonacciSpecs.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace FibonacciSpecs {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
