31152dac971d388b48532e05f16475dc94a21ef1
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-03-29 18:26:32 +0100

    calculate_fifth_element

--- Fibonacci/Fibonacci/FibonacciSpecs.cs
+++ Fibonacci/Fibonacci/FibonacciSpecs.cs
@@ -42,16 +42,21 @@ namespace FibonacciSpecs {
             Assert.AreEqual(expected, number);
         }
 
+        [Test]
+        public void calculate_fifth_element() {
+            var expected = 3;
 
+            var number = Fibonacci.Run(4);
+
+            Assert.AreEqual(expected, number);
+        }
     }
 
     public class Fibonacci {
         public static int Run(int position) {
-            if (position > 0) {
-                if (position > 2) return 2;
-                return 1;
-            }
-            return 0;
+            if (position == 0) return 0;
+            if (position == 1) return 1;
+            return Run(position - 1) + Run(position - 2);
         }
     }
 }
