f5294062289757368a88bfd95a84034bf58eb7fd
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-03-29 18:20:39 +0100

    calculate_third_element

--- Fibonacci/Fibonacci/FibonacciSpecs.cs
+++ Fibonacci/Fibonacci/FibonacciSpecs.cs
@@ -23,11 +23,20 @@ namespace FibonacciSpecs {
 
             Assert.AreEqual(expected, number);
         }
+
+        [Test]
+        public void calculate_third_element() {
+            var expected = 1;
+
+            var number = Fibonacci.Run(2);
+
+            Assert.AreEqual(expected, number);
+        }
     }
 
     public class Fibonacci {
         public static int Run(int position) {
-            if (position == 1) return 1;
+            if (position > 0 ) return 1;
             return 0;
         }
     }
